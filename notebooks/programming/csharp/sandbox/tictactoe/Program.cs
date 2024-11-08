47bb05e3ed71d817cea2f7b91e7b1fda95c119d6
Johannes Glück <glueck@htw-berlin.de>
2024-11-08 16:07:07 +0100

    Result of lecture 4

--- notebooks/programming/csharp/sandbox/tictactoe/Program.cs
+++ notebooks/programming/csharp/sandbox/tictactoe/Program.cs
@@ -4,18 +4,83 @@ class Program
 {
     static void Main(string[] args)
     {
+        const int maxRunden = 9;
+        const string zeilenTrenner = "#---+---+---#";
+
         // Spielfeld initialisieren -> öffne CSharp_Loops_TicTacToe.de.html in Projektmappenelementen (Rechtsklick in Browser anzeigen)
+        string[,] spielfeld = {
+                                { " ", " ", " " },
+                                { " ", " ", " " },
+                                { " ", " ", " " }
+                              };
 
         // Spielablauf  -> öffne CSharp_Loops_TicTacToe.de.html
+        string spieler1 = "X";
+        string spieler2 = "O";
+        string aktuellerSpieler = spieler1;
 
-        // Spielfeld anzeigen -> öffne Console_WriteLine_TicTacToe.de.html
+        for (int runde = 0; runde < maxRunden; runde++) // bool gameover = false; while (!gameover) {}
+        {
+            // Aktuellen Spieler anhand von Runde setzen (Variante 1)
+            // if (runde % 2 == 0) aktuellerSpieler = spieler1; // % ist Modulo -> öffne Number_Systems.de.html
+            // else aktuellerSpieler = spieler2;
 
-        // Spielerzug -> öffne Console_ReadLine_Basics.de.html
+            // Console.Clear();
 
-        // Sieg überprüfen -> öffne CSharp_Conditional_TicTacToe.de.html
+            // Spielfeld anzeigen -> öffne Console_WriteLine_TicTacToe.de.html
+            Console.WriteLine("  A   B   C");
+            Console.WriteLine(zeilenTrenner);
 
-        // Unentschieden überprüfen -> öffne CSharp_Conditional_TicTacToe.de.html
+            for (int i = 0; i < 3 /* todo: später optimieren */; i++)
+            {
+                Console.Write("|");
+                for (int j = 0; j < 3 /* todo: später optimieren */; j++)
+                {
+                    Console.Write($" {spielfeld[i, j]} |");
+                }
+                Console.Write($" {i + 1}\n");
+                Console.WriteLine(zeilenTrenner);
+            }
+            /***************************************************************/
 
-        // Spieler wechseln -> öffne CSharp_Other_Operators_Basics.de.html
+            // Spielerzug -> öffne Console_ReadLine_Basics.de.html todo: Eingabe als Koordinate z.B. a1
+            bool korrekteEingabe = false;
+            do
+            {
+                Console.WriteLine($"Spieler {aktuellerSpieler}: bitte gib die gewünschte Zeile an (z.B. \"1\"):");
+                string eingabeZeile = Console.ReadLine();
+                /* todo: Fehlerprüfung fehlt*/
+                int zeile = int.Parse(eingabeZeile) - 1;
+
+                Console.WriteLine($"Spieler {aktuellerSpieler}: bitte gib die gewünschte Spalte an (z.B. \"A\"):");
+                string eingabeSpalte = Console.ReadLine();
+                eingabeSpalte = eingabeSpalte.ToUpper();
+                /* todo: Fehlerprüfung fehlt*/
+                // int spalte = eingabeSpalte[0] - 'A'; // Alternative ist:
+                int spalte = 0;
+                if (eingabeSpalte == "B") spalte = 1;
+                else if (eingabeSpalte == "C") spalte = 2;
+
+                Console.WriteLine($"Danke, die Eingabe enstpricht den Koordinaten: {zeile}, {spalte}.");
+                if (spielfeld[zeile, spalte] == " ")
+                {
+                    spielfeld[zeile, spalte] = aktuellerSpieler;
+                    korrekteEingabe = true;
+                }
+                else
+                {
+                    Console.WriteLine($"Das Feld \"{eingabeZeile}, {eingabeSpalte}\" ist leider schon belegt, bitte versuches es gleich noch einmal!");
+                }
+            }
+            while (!korrekteEingabe);
+
+            // Sieg überprüfen -> öffne CSharp_Conditional_TicTacToe.de.html
+
+            // Unentschieden überprüfen -> öffne CSharp_Conditional_TicTacToe.de.html
+
+            // Spieler wechseln -> öffne CSharp_Other_Operators_Basics.de.html (Variante 2)
+            if (aktuellerSpieler == spieler1) aktuellerSpieler = spieler2;
+            else aktuellerSpieler = spieler1;
+        }
     }
 }
