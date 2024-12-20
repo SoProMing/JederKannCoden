2424ed0cbe2b0069db9a3f86c74d257dfc8b1687
Johannes Glück <glueck@htw-berlin.de>
2024-12-20 17:20:42 +0100

    First version of TicTacToe OOP

---
+++ notebooks/programming/csharp/sandbox/TicTacToeOOP/Spieler.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToeOOP
+{
+    internal class Spieler
+    {
+        public string Stein { get; init; }
+
+        public string Name { get; init; }
+
+        public Spieler( string Name, string Stein)
+        {
+            this.Name = Name;
+            this.Stein = Stein;
+        }
+    }
+}
