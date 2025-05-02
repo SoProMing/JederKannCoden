c95aff7d12d738ccceb7b18b1434388c40326614
Johannes Glück <glueck@htw-berlin.de>
2025-05-02 12:40:20 +0200

    Added class cell

---
+++ notebooks/programming/csharp/sandbox/WasMachenWirDenn/WasMachenWirDenn/Model/Cell.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WasMachenWirDenn.Model
+{
+    public class Cell : INotifyPropertyChanged
+    {
+        private string content;
+
+        public string Content
+        {
+            get => content;
+            set
+            {
+                if (content != value)
+                {
+                    content = value;
+                    OnPropertyChanged(nameof(Content));
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string propertyName) =>
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
