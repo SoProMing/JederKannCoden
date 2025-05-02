648ec0aeb690eb5fb272a8778eae593427ae02b4
Johannes Glück <glueck@htw-berlin.de>
2025-05-02 12:43:11 +0200

    Added GameViewModel

---
+++ notebooks/programming/csharp/sandbox/WasMachenWirDenn/WasMachenWirDenn/ViewModel/GameViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WasMachenWirDenn.Model;
+
+namespace WasMachenWirDenn.ViewModel
+{
+    public class GameViewModel : INotifyPropertyChanged
+    {
+        public ObservableCollection<Cell> Board { get; }
+        private string currentPlayer = "X";
+
+        public GameViewModel()
+        {
+            Board = new ObservableCollection<Cell>();
+            for (int i = 0; i < 9; i++)
+                Board.Add(new Cell());
+        }
+
+        public void CellClicked(Cell cell)
+        {
+            if (string.IsNullOrEmpty(cell.Content))
+            {
+                cell.Content = currentPlayer;
+                currentPlayer = currentPlayer == "X" ? "O" : "X";
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string propertyName) =>
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
