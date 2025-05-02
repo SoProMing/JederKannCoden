8b081d0ea7d7fd566ba832c874f96d1081642c84
Johannes Glück <glueck@htw-berlin.de>
2025-05-02 15:25:51 +0200

    Intermediate state

---
+++ notebooks/programming/csharp/sandbox/WasMachenWirDenn/WasMachenWirDenn/Feld.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WasMachenWirDenn
+{
+    class Feld : INotifyPropertyChanged
+    {
+        string inhalt;
+        public string Inhalt
+        {
+            get => inhalt;
+            set {
+                if (value != inhalt)
+                {
+                    inhalt = value;
+                    OnPropertyChanged(nameof(Inhalt));
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string propertyName) =>
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
---
+++ notebooks/programming/csharp/sandbox/WasMachenWirDenn/WasMachenWirDenn/SpielViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WasMachenWirDenn
+{
+    class SpielViewModel : INotifyPropertyChanged // todo: try without notification
+    {
+        ObservableCollection<Feld> Spielfeld { get;  }
+
+        public SpielViewModel()
+        {
+            Spielfeld = new ObservableCollection<Feld>();
+            for (int i = 0; i < 36; i++)
+            {
+                Spielfeld.Add(new Feld());
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string propertyName) =>
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
